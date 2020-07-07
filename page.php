7c1333901b0b5b3d1613363f47266804bb348750
Morgan Spencer <morgan@spencercreative.co>
2020-07-06 21:31:55 -0400

    Remove sidebars from pages

--- page.php
+++ page.php
@@ -29,8 +29,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<div class="row">
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+			<?php if ( is_front_page() || is_home() ) : ?>
+				<!-- Do the left sidebar check -->
+				<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
+			<?php endif; ?>
 
 			<main class="site-main" id="main">
 
@@ -49,8 +51,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 			</main><!-- #main -->
 
-			<!-- Do the right sidebar check -->
-			<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+			<?php if ( is_front_page() || is_home() ) : ?>
+				<!-- Do the right sidebar check -->
+				<?php get_template_part( 'global-templates/right-sidebar-check' ); ?>
+			<?php endif; ?>
 
 		</div><!-- .row -->
 
